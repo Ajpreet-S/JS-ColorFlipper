c405182bf5522f912839ea4d4d530bd88a7385f5
Ajpreet Singh <SINGHAG19@juniata.edu>
2023-04-01 22:33:23 -0400

    feat: flipper will now always apply a different bg color

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function selectRandomElement(arr) {
+    return arr[getRandomInt(arr.length)];
+}
+
 // Valid CSS background colors
 const colors = [
     'black',
@@ -22,12 +26,22 @@ const colors = [
     'aqua',
 ];
 
-// The button that will
+
 const button = document.getElementById('btn');
 const background = document.getElementById('test');
-let bgColorText = document.getElementById('bgcolortext');
+let text = document.getElementById('bgcolortext');
 button.addEventListener('click', () => {
-    const randomColor = colors[getRandomInt(colors.length)];
+    // Select a random color
+    let randomColor = selectRandomElement(colors);
+
+    // If the random color is the same as the background's current color, get another random one
+    while (randomColor === background.style.backgroundColor) {
+        randomColor = selectRandomElement(colors);
+    }
+
+    // Apply random color to background
     background.style.backgroundColor = randomColor;
-    bgColorText.innerText = randomColor;
+
+    // Display color's name
+    text.innerText = randomColor;
 })
